e3bbcb09c7da68cf3528d7f068f80655731f97c2
Victor Tortolero <victor.tortolero@wizeline.com>
2020-02-09 14:41:37 -0600

    Add Theme space scale and font sizes

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+import { Theme } from './types';
+
+const theme: Theme = {
+  space: {
+    none: 0,
+    xsmall: 4,
+    small: 8,
+    medium: 16,
+    large: 32,
+    xlarge: 64
+  },
+  fontSizes: {
+    xsmall: 12,
+    small: 14,
+    medium: 16,
+    large: 20,
+    xlarge: 32
+  }
+};
+
+export default theme;
---
+++ src/styles/themes/types.d.ts
@@ -0,0 +1,19 @@
+export type SpaceScaleUnit =
+  | 'none'
+  | 'xsmall'
+  | 'small'
+  | 'medium'
+  | 'large'
+  | 'xlarge';
+
+export type FontSizeScaleUnit =
+  | 'xsmall'
+  | 'small'
+  | 'medium'
+  | 'large'
+  | 'xlarge';
+
+type Theme = {
+  space: Record<SpaceScaleUnit, number>;
+  fontSizes: Record<FontSizeScaleUnit, number>;
+};
